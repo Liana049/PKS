3c7e184b1d0165903929316bfcfcae438d466d3a
Liana049 <61280532+Liana049@users.noreply.github.com>
2020-05-11 22:06:22 +0700

    Add files via upload

---
+++ kelompok 3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int a, b;
+	cin>>a>>b;
+	 
+	if(a>b)
+	{
+		cout<<"a lebih besar dari b";
+	}else if(a<b)
+	{
+		cout<<"a lebih kecil dari b";
+	}else{
+		cout<<"a sama dengan b";
+	}
+}
