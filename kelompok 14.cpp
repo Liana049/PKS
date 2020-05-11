0239542ae549041758125718e98f758e3cb05574
Liana049 <61280532+Liana049@users.noreply.github.com>
2020-05-11 22:07:45 +0700

    Add files via upload

---
+++ kelompok 14.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, jumlah;
+	cin>>a>>b;
+	
+	jumlah= a+b;
+	
+	if(jumlah>90)
+	{
+		cout<<"bintang tidak pernah terbenam";
+	}else{
+		cout<<"bintang terbit dan teerbenam";
+	}
+}
